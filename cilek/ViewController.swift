67770fe9c5675f0cfecc2bcf9c6a1b6b77088109
Sudenaz Civelek <119611921+sudenazcivelek@users.noreply.github.com>
2023-06-24 21:52:37 +0300

    Initial Commit

---
+++ cilek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cilek
+//
+//  Created by sude on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
